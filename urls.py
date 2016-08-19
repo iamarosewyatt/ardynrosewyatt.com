d9729a442a19c2aa7c57887f940e1252f53c3289
Ryan Zander <exide@hotmail.com>
2016-08-19 15:01:48 -0700

    Added site scaffolding based on mock ups.

--- urls.py
+++ urls.py
@@ -1,6 +1,10 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls)
+    url(r'^admin/', admin.site.urls),
+    url(r'^', include('home.urls')),
+    url(r'^about/', include('about.urls')),
+    url(r'^journal/', include('journal.urls')),
+    url(r'^good-stuff/', include('good_stuff.urls')),
 ]
