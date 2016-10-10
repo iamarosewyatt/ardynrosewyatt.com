6218fc1a225faea226c49fc8878910be409b8135
Ryan Zander <exide@hotmail.com>
2016-10-09 21:36:56 -0700

    Added a single post view using a basic id as the key.

--- journal/views.py
+++ journal/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 from journal.models import Post
 
@@ -5,3 +6,11 @@ from journal.models import Post
 def index(request):
     posts = Post.objects.all()
     return render(request, 'journal.html', {'posts': posts})
+
+
+def post(request, post_id):
+    try:
+        p = Post.objects.get(pk=post_id)
+    except Post.DoesNotExist:
+        raise Http404("No post exists with that ID")
+    return render(request, 'post.html', {'post': p})
