e80dc30347ca6901ea5d953c0169922a0340bcda
Ryan Zander <exide@hotmail.com>
2016-10-13 10:31:12 -0700

    Setup a proper asset/static structure.

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -105,4 +105,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = ('static',)
+STATICFILES_DIRS = ('assets',)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
