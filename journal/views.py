69b7fa179cffebd6fa57aaa22211c7477189e831
Ryan Zander <exide@hotmail.com>
2016-10-13 13:31:27 -0700

    Added tag navigation, ordering, and other creature comforts.

--- journal/views.py
+++ journal/views.py
@@ -8,7 +8,7 @@ from tagging.models import Tag, TaggedItem
 
 
 def index(request):
-    posts = Post.objects.all()
+    posts = Post.objects.all().order_by('-created')
     tags = [_build_tagged_posts(tag) for tag in Tag.objects.usage_for_model(Post)]
     output = {
         'tags': tags,
@@ -20,37 +20,43 @@ def index(request):
 def _build_tagged_posts(tag):
     return {
         'name': tag.name,
-        'posts': TaggedItem.objects.get_by_model(Post, tag)
+        'posts': TaggedItem.objects.get_by_model(Post, tag).order_by('-created')
     }
 
 
 def by_year(request, year):
-    posts = Post.objects.filter(created__year=year)
+    posts = Post.objects.filter(created__year=year).order_by('-created')
     output = {
-        'period': year,
+        'filter': year,
         'posts': posts
     }
     return render(request, 'archive.html', output)
 
 
 def by_month(request, year, month):
-    posts = Post.objects.filter(created__year=year,
-                                created__month=month)
+    posts = Post.objects.filter(created__year=year, created__month=month).order_by('-created')
     timestamp = date(year=int(year), month=int(month), day=1)
     output = {
-        'period': formats.date_format(timestamp, "F Y"),
+        'filter': formats.date_format(timestamp, "F Y"),
         'posts': posts
     }
     return render(request, 'archive.html', output)
 
 
 def by_day(request, year, month, day):
-    posts = Post.objects.filter(created__year=year,
-                                created__month=month,
-                                created__day=day)
+    posts = Post.objects.filter(created__year=year, created__month=month, created__day=day).order_by('-created')
     timestamp = date(year=int(year), month=int(month), day=int(day))
     output = {
-        'period': formats.date_format(timestamp, "F jS, Y"),
+        'filter': formats.date_format(timestamp, "F jS, Y"),
+        'posts': posts
+    }
+    return render(request, 'archive.html', output)
+
+
+def by_tag(request, tag):
+    posts = TaggedItem.objects.get_by_model(Post, tag)
+    output = {
+        'filter': tag,
         'posts': posts
     }
     return render(request, 'archive.html', output)
@@ -58,10 +64,7 @@ def by_day(request, year, month, day):
 
 def post(request, year, month, day, post_id):
     try:
-        p = Post.objects.get(created__year=year,
-                             created__month=month,
-                             created__day=day,
-                             id=post_id)
+        p = Post.objects.get(created__year=year, created__month=month, created__day=day, id=post_id)
     except Post.DoesNotExist:
         raise Http404("No post exists with that ID")
     return render(request, 'post.html', {'post': p})
--- settings.py
+++ settings.py
@@ -55,7 +55,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.template.context_processors.media',
                 'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+                'django.contrib.messages.context_processors.messages'
             ],
         },
     },
