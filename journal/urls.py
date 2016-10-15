ce1793d81adc602ae8475609dd243cd46320a870
Ryan Zander <exide@hotmail.com>
2016-10-14 23:09:38 -0700

    Made better use of the URL naming.

--- journal/urls.py
+++ journal/urls.py
@@ -3,10 +3,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='journal.views.index'),
-    url(r'(?P<year>[0-9]{4})/$', views.by_year, name='journal.views.by_year'),
-    url(r'(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/$', views.by_month, name='journal.views.by_month'),
-    url(r'(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2})/$', views.by_day, name='journal.views.by_day'),
-    url(r'(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2})/(?P<slug>.+)$', views.post, name='journal.views.post'),
-    url(r'tag/(?P<tag>.+)$', views.by_tag, name='journal.views.by_tag')
+    url(r'^$', views.index, name='index'),
+    url(r'(?P<year>[0-9]{4})/$', views.by_year, name='by_year'),
+    url(r'(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/$', views.by_month, name='by_month'),
+    url(r'(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2})/$', views.by_day, name='by_day'),
+    url(r'(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2})/(?P<slug>.+)$', views.post, name='post'),
+    url(r'tag/(?P<tag>.+)$', views.by_tag, name='by_tag')
 ]
--- urls.py
+++ urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('home.urls')),
     url(r'^about/', include('about.urls')),
-    url(r'^journal/', include('journal.urls')),
+    url(r'^journal/', include('journal.urls'), name='journal'),
     url(r'^cool-stuff/', include('cool_stuff.urls')),
     url(r'^whats-coming/', include('whats_coming.urls')),
     url(r'^book/', include('book.urls')),
