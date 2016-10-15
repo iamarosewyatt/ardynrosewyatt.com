6e0b75213f4cde82c49685986eea8812526cb47b
Ryan Zander <exide@hotmail.com>
2016-10-15 13:27:41 -0700

    Fixed a bug in the URL regex.

--- journal/urls.py
+++ journal/urls.py
@@ -4,8 +4,8 @@ from journal import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'(?P<year>[0-9]{4})/$', views.by_year, name='by_year'),
-    url(r'(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/$', views.by_month, name='by_month'),
-    url(r'(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2})/$', views.by_day, name='by_day'),
-    url(r'(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2})/(?P<slug>.+)$', views.post, name='post'),
+    url(r'(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$', views.by_month, name='by_month'),
+    url(r'(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/(?P<day>[0-9]{1,2})/$', views.by_day, name='by_day'),
+    url(r'(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/(?P<day>[0-9]{1,2})/(?P<slug>.+)$', views.post, name='post'),
     url(r'tag/(?P<tag>.+)$', views.by_tag, name='by_tag')
 ]
