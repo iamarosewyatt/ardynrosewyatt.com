0728d0f0df31a810b081483bd1fab4274f445029
Ryan Zander <exide@hotmail.com>
2016-10-21 22:52:24 -0700

    Tied in the newsletter sign-up form.

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,5 @@ from django.conf.urls import url
 from home import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^newsletter', views.newsletter, name='newsletter')
+    url(r'^$', views.index, name='index')
 ]
--- home/views.py
+++ home/views.py
@@ -3,7 +3,3 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'home.html')
-
-
-def newsletter(request):
-    return render(request, 'newsletter.html')
