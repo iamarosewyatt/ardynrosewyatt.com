3ac11ba64a5d31e33041eff74d9b0cee577c8085
Ryan Zander <exide@hotmail.com>
2016-10-28 22:25:11 -0700

    Fixed an echo statement.

--- populate-database.sh
+++ populate-database.sh
@@ -9,5 +9,5 @@ ${CMD} migrate --run-syncdb
 ${CMD} loaddata home/fixtures/sites.json
 ${CMD} loaddata about/fixtures/moments.json
 ${CMD} loaddata journal/fixtures/posts.json
-echo Creating a super user account (user: iamarosewyatt) ...
+echo "Creating a super user account (user: iamarosewyatt) ..."
 ${CMD} createsuperuser --username "iamarosewyatt" --email "arose@ardynrosewyatt.com"
