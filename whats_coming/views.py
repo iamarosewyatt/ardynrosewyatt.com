006f77d74528690f2b3f195687c8938c3feac459
Ryan Zander <exide@hotmail.com>
2016-10-28 22:18:22 -0700

    Changed the name of a template to be more explicit.

--- whats_coming/views.py
+++ whats_coming/views.py
@@ -4,4 +4,4 @@ from whats_coming.models import Item
 
 def index(request):
     items = Item.objects.all().order_by('-created')
-    return render(request, 'index.html', {'items': items})
+    return render(request, 'whats-coming.html', {'items': items})
