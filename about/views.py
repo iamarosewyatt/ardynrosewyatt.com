33673db44e0d9e0ad38531ff588f1f2ad470972d
Ryan Zander <exide@hotmail.com>
2016-11-07 20:12:26 -0800

    Fixed ordering of moments.

--- about/views.py
+++ about/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 
 
 def index(request):
-    moments = Moment.objects.all().order_by('when')
+    moments = Moment.objects.all().order_by('order')
     photos = [moment.image for moment in moments if moment.image]
     return render(request, 'about.html', {'moments': moments, 'photos': photos})
